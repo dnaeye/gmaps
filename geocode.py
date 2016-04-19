29c265d357f3c7fca7ff30e15dbf9e502c297b87
Dan Hong <dnaeye@gmail.com>
2016-04-19 08:23:17 -0700

    Update geocode.py

--- geocode.py
+++ geocode.py
@@ -2,10 +2,11 @@
 """
 Created on Thu Apr 14 07:00:36 2016
 
-AddressPostalCode retrieves the postal code for addresses using the Google Maps Geocoding API.
+GeocodeAddress retrieves the postal code for addresses using the Google Maps Geocoding API.
 https://developers.google.com/maps/documentation/geocoding/
 
-Note: Free API requests are limited to 2,500 per day. See https://developers.google.com/maps/documentation/geocoding/usage-limits
+Note: Free API requests are limited to 2,500 per day. 
+See https://developers.google.com/maps/documentation/geocoding/usage-limits
 for more info.
 
 @author: dhong
@@ -22,7 +23,7 @@ with open(r"D:\Users\dhong\Documents\Code\Python\addresses in.csv","r") as infil
     
     # create output file for trip data via Google Maps API
     outfile = open(r"D:\Users\dhong\Documents\Code\Python\addresses out.csv","wb")
-    writer = csv.DictWriter(outfile, fieldnames=['Customer Num','Company','Shipping Address','City','State','Postal Code','County','Latitude','Longitude'], delimiter=',')
+    writer = csv.DictWriter(outfile, fieldnames=['Customer Num','Company','Shipping Address','City','State','Postal Code','County','Latitude','Longitude','Partial Match'], delimiter=',')
     writer.writeheader()
 
     # initialize temporary postal code variables
@@ -33,38 +34,52 @@ with open(r"D:\Users\dhong\Documents\Code\Python\addresses in.csv","r") as infil
     # loop through each store in import file to retrieve geocode data from Google    
     for row in reader:
         
-        # clean shipping address
+        # identify portion of shipping address that is relevant for geocoding
         street_address_length = len(row['Shipping Address'])
         street_address_number = re.search('\d',row['Shipping Address'])
-        street_address_number = street_address_number.start()
+        
+        # find index of street address number; if none, start at 0
+        try:
+            street_address_number = street_address_number.start()
+        except:
+            street_address_number = 0
+            
+        # create cleaned shipping address
         row['Shipping Address'] = row['Shipping Address'][street_address_number:street_address_length].title()
         
         # call Google Maps API Geocode function
         address = row['Shipping Address'] + ',' + row['City'] + ',' + row['State']
-        result = gmaps.geocode(address)
         
-        # loop through JSON to find city, county, postal code data
-        for dct in result[0]['address_components']:
-            if dct['types'][0] == 'administrative_area_level_2':
-                county = dct['long_name']
-            if dct['types'][0] == 'postal_code':
-                postal_code_base = dct['long_name']
-            if dct['types'][0] == 'postal_code_suffix':
-                postal_code_suffix = "-" + dct['long_name']
-                  
-        row['County'] = county
-        row['Postal Code'] = postal_code_base + postal_code_suffix
-
-        # check if geocode found matching address or not
-#        if 'partial_match' in result[0]:
-#            row['Latitude'] = ''
-#            row['Longitude'] = ''
-#        else:
-#            row['Latitude'] = result[0]['geometry']['location']['lat']
-#            row['Longitude'] = result[0]['geometry']['location']['lng']
-
-        row['Latitude'] = result[0]['geometry']['location']['lat']
-        row['Longitude'] = result[0]['geometry']['location']['lng']
+        try:        
+            result = gmaps.geocode(address)
+        
+            # loop through JSON to find city, county, postal code data
+            for dct in result[0]['address_components']:
+                if dct['types'][0] == 'administrative_area_level_2':
+                    county = dct['long_name']
+                if dct['types'][0] == 'postal_code':
+                    postal_code_base = dct['long_name']
+                if dct['types'][0] == 'postal_code_suffix':
+                    postal_code_suffix = "-" + dct['long_name']
+    
+            # set geocode data                  
+            row['County'] = county
+            row['Postal Code'] = postal_code_base + postal_code_suffix
+            row['Latitude'] = result[0]['geometry']['location']['lat']
+            row['Longitude'] = result[0]['geometry']['location']['lng']
+            
+            # check if geocode is an exact match
+            if 'partial_match' in result[0]:
+                row['Partial Match'] = 1
+            else:
+                row['Partial Match'] = 0
+        
+        except:
+            row['County'] = ''
+            row['Postal Code'] = ''
+            row['Latitude'] = ''
+            row['Longitude'] = ''
+            row['Partial Match'] = 'INVALID'
 
         # output to console for diagnostics
         print row
@@ -78,7 +93,7 @@ with open(r"D:\Users\dhong\Documents\Code\Python\addresses in.csv","r") as infil
         county = ''
                 
         # pause between API requests per Google Maps API limitation
-        time.sleep(1)
+        #time.sleep(1)
         
     # close connections to both input and output files    
     infile.close()
